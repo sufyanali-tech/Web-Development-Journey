e956997f512299c106ade1479edc48ed14f05c0a
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-13 10:38:03 +0500

    Variable and Values in JavaScript

---
+++ JavaScript/Variable_and_Value/script.js
@@ -0,0 +1,11 @@
+// Values are the Fundamental Data Store by Variable e.g "Sufyan Ali"
+// Variable are like Which the Value and give them a certain name e.g let firstName = "Sufyan"
+
+let country = "Pakistan";
+let continent = "Asia";
+let populationInMillions = 24
+
+console.log(country);
+console.log(continent);
+console.log(populationInMillions);
+
