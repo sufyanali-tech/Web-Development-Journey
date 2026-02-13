71d5999eeb9ed8abc786b22f2d2c901e41eccb7d
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-13 11:17:05 +0500

    Data Types in JavaScript

---
+++ JavaScript/DataTypes_in_Js/script.js
@@ -0,0 +1,38 @@
+// Value are store in Js in mainly two types Primative and Obect
+
+// There are two types of datatypes in Js Primative and Object
+// Primative are further more 7 types eg 
+// Number let num = 20 , 2.45 -> also Number
+// String let firstName = "Sufyan Ali"
+// Boolean let value = true or false
+// Undefined let lastName;
+// Null let a = null;
+// Symbol
+// Bigint Number storing the simple Number variable not hold the value
+
+let myNumber = 20;
+console.log(myNumber);
+console.log(typeof myNumber)
+
+
+let firstName = "Sufyan"
+console.log(firstName);
+console.log(typeof firstName)
+
+
+let boolValue = true;
+console.log(boolValue);
+console.log(typeof boolValue)
+
+
+let notAssigning;
+console.log(notAssigning);
+console.log(typeof notAssigning)
+
+
+let nullValue = null;
+console.log(nullValue);
+console.log(typeof nullValue)
+
+
+// type of is a special type of function which check the type of the variable or Value Js is Dynamically assign the types of the varaible according to their values
