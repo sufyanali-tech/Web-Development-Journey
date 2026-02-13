cddfce1704c9374682ce3605070ef7199e51080b
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-13 11:43:04 +0500

    Let Const Var Difference in Js

---
+++ JavaScript/let_const_var_in_Js/script.js
@@ -0,0 +1,28 @@
+// let , const , var are defining the variables but there slightly difference between them 
+
+// let ( is mutuble or reassigable in program change the value when we want)
+
+let language = "English";
+console.log(language);
+
+// Ressigning the Value 
+
+language = "Urdu";
+console.log(language);
+
+// const ( is immutable connot change value if once declare them)
+
+const firstName = "Sufyan";
+console.log(firstName);
+
+// firstName = "Ali"   Give the Error const can't be change when declare one time
+// console.log(firstName);
+
+// var  ( var is a old way to declare the variable but after ECS6+ Update now we are using the let 
+// var can't give your error when variable once declare once time then you declare again them)
+
+var lastName = "Ali"
+console.log(lastName);
+
+// var lastName = "d"
+// console.log(lastName); Can't give you error
