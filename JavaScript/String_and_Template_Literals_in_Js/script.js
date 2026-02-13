1ecb8c90eab045c22b41e8eb012d7149d4d1bcba
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-14 03:35:46 +0500

    String and Template Literals in Js

---
+++ JavaScript/String_and_Template_Literals_in_Js/script.js
@@ -0,0 +1,13 @@
+// String in Js 
+
+const myName = "Sufyan Ali"
+const workDoing = "Student"
+
+console.log("My name is " + myName + " and I'm a " + workDoing);
+
+// Templete Literals in Js
+
+const learning = "JavaScript"
+const develop = "Web Applications"
+
+console.log(`I'm Learning ${learning} to build ${develop}`);
