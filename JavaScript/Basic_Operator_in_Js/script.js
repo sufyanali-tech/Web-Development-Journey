a32277e7168395165c131b16cf78b055010bd864
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-13 12:29:54 +0500

    Basic Operator in Js

---
+++ JavaScript/Basic_Operator_in_Js/script.js
@@ -0,0 +1,23 @@
+// Math Operator in Js + - * /
+
+const storeValue = 2 + 2;
+console.log(storeValue);
+
+const storeAnotherValue = storeValue - 1;;
+console.log(storeAnotherValue);
+
+// Assignmeant Operator in Js (=)
+
+let x = 10;
+x += 5; // x = x + 5
+console.log(x);
+
+let y = 20;
+y *= 2; // y = y * 2;
+console.log(y);
+
+// Comparison Operator > < >= <= return Boolean Value
+
+console.log(y > x);
+console.log(x > y);
+
--- JavaScript/Variable_and_Value_in_Js/script.js
+++ JavaScript/Variable_and_Value_in_Js/script.js
@@ -1,7 +1,7 @@
 // Values are the Fundamental Data Store by Variable e.g "Sufyan Ali"
 // Variable are like Which the Value and give them a certain name e.g let firstName = "Sufyan"
 // Usually we are using name for the variable in Js is camelCase like firstName, lastName etc
-
+// Only use 
 
 let country = "Pakistan";
 let continent = "Asia";
