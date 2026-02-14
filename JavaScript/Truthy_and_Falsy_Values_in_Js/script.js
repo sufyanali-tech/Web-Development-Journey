c983f1de4d4327917a1425c65299b1f21029c225
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-14 10:03:47 +0500

    Falsy and Truthy Values in Js

---
+++ JavaScript/Truthy_and_Falsy_Values_in_Js/script.js
@@ -0,0 +1,9 @@
+// falsy values in 5 cases : 0 , "" , undefined , null , NaN
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+
+//  Except these all are the Truthy Values
