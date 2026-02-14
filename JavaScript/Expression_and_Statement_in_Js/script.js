7c662af3afd63ce4c8e81401e9f32629cbeba8ad
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-14 19:10:35 +0500

    Expression and Statement in Js

---
+++ JavaScript/Expression_and_Statement_in_Js/script.js
@@ -0,0 +1,19 @@
+// Expression 
+
+// Expression is a lines of code which produce some value For Example
+
+const value = 2 + 2;   // 2 + 2 Produce some value this is called Expression
+
+// Statement 
+
+// Statement is also a big lines of codes which are like full sentence that translate our actions For Example
+
+if (value > 1) {
+    
+    console.log("Value is greater than 1");
+    
+} else {
+
+    console.log("Value is not greater than 1"); // This statement tell us something
+    
+}
