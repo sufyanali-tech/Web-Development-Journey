80ddf95bee813e9cfe60e1acbfa785937e138ba4
Sufyan Ali <sufyanali.dev@gmail.com>
2026-02-15 14:39:09 +0500

    Strict Mode is Js

---
+++ JavaScript_Fundamental_Part_2/Strict_Mode_Enable_in_Js/script.js
@@ -0,0 +1,11 @@
+"use strict"; // if we don't use the strict mode then javaScript will not show the error in console which cause the accidental error in out program
+
+let variable = "Sufyan Ali";
+
+if(variable) {
+
+    variables = "Ali"  
+}
+
+console.log(variable);
+
