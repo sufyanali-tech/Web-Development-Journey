d007bab1073264355df4e3a2601cf7bf1583d028
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-04 08:07:49 +0500

    Some Array Questions

--- Practicing_All_JS_Fundamental_Questions/script.js
+++ Practicing_All_JS_Fundamental_Questions/script.js
@@ -1,17 +1,71 @@
-// Question Number 1
+// // Question Number 1
 
-function counter() {
+// function counter() {
 
-    let count = 0;
-    let myName = "Sufyan"
-    return function increament() {
+//     let count = 0;
+//     let myName = "Sufyan"
+//     return function increament() {
 
-        count++;
-        return {count,myName}
-    }
-}
+//         count++;
+//         return {count,myName}
+//     }
+// }
 
-const myCounter = counter();
-console.log(myCounter());
-console.log(myCounter());
-console.log(myCounter());
+// const myCounter = counter();
+// console.log(myCounter());
+// console.log(myCounter());
+// console.log(myCounter());
+
+
+// Question 2 From Array
+
+let arr = [1,2,3,4,5]
+
+console.log(arr);
+
+let pop = arr.pop()
+console.log(arr);
+
+let push = arr.push(7)
+console.log(arr);
+
+let shift = arr.shift();
+console.log(arr);
+let unshift = arr.unshift(1);
+console.log(arr);
+
+let splice = arr.splice(0,1);
+console.log(arr);
+
+console.log(arr);
+
+let slice = arr.slice(0,1)
+
+console.log(slice);
+
+console.log(arr);
+
+let array = [1,2,3,4,5];
+console.log(array);
+
+console.log(array.reverse());
+
+let assendingOrder = array.sort(function(a,b) {
+
+    return a - b;
+})
+
+console.log(assendingOrder);
+
+console.log(array);
+
+let dessendingOrder = array.sort(function(a,b) {
+
+    return b - a;
+})
+
+console.log(dessendingOrder);
+
+console.log(array);
+
+
