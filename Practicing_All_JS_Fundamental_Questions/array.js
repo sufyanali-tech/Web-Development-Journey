5010845353c794bfeed966a3e230e441c4060103
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-04 09:00:20 +0500

    For each method in array in Js

--- Practicing_All_JS_Fundamental_Questions/array.js
+++ Practicing_All_JS_Fundamental_Questions/array.js
@@ -1,7 +1,22 @@
-let array = [1,2,3,4,5];
+// Using For Each Array 
 
-let result = array.forEach(function(val){
+let newArray = [1,2,3,4,5];
 
-    console.log(val+5);
-})
-12
+let nextArray = newArray.forEach((value) => { return value * 2})
+
+console.log(nextArray); // Not returning new Array
+
+newArray.forEach((value) => { console.log(value * 2)}) // Only changes Occur 
+
+
+// Using Map Method in Array 
+
+// let array = [1,2,3,4,5];
+
+// let result = array.map(function(value) {
+
+//     return value * 2
+// })
+
+// console.log(result); // Returning New Array
+// console.log(array);
---
+++ Practicing_All_JS_Fundamental_Questions/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let array = [1,2,3,4,5];
+
+// let result = array.map(function(value) {
+
+//     return value * 2
+// })
+
+// console.log(result); // Returning New Array
+// console.log(array);
