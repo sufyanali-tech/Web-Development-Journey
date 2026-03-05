cabc46c4170ac56bff265bce17d8736419d4e199
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-05 15:33:43 +0500

    Objects keys in js

---
+++ Practicing_All_JS_Fundamental_Questions/objects_keys.js
@@ -0,0 +1,11 @@
+// All keys of the object enter in the array
+
+let obj = {
+    myName : "Sufyan Ali",
+    age : 19,
+    email : "test@test.com"
+}
+
+let object_keys = Object.keys(obj);
+
+console.log(object_keys);
