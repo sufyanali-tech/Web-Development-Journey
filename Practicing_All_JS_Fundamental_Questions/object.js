38330bccb13f733fdef41b3bb0f30dab8555dc2f
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-05 14:59:03 +0500

    Object in JS

---
+++ Practicing_All_JS_Fundamental_Questions/object.js
@@ -0,0 +1,14 @@
+// Basic Syntax of object in JavaScript
+
+let obj = {
+    myName : "Sufyan Ali",
+    age : 19,
+    hobbies : "Cricket"
+}
+
+// 1st Method to access the object using (.)
+console.log(obj.myName,obj.age,obj.hobbies);
+console.log(obj);
+
+// 2nd Method to access the object in JavaScript
+console.log(obj["myName"]);
