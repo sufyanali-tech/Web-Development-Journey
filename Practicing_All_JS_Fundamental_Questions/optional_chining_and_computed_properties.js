083d26e0ba93f2d48290d122237b8b447fa517a8
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-05 16:06:10 +0500

    Computed Properties in Js

--- Practicing_All_JS_Fundamental_Questions/optional_chining_and_computed_properties.js
+++ Practicing_All_JS_Fundamental_Questions/optional_chining_and_computed_properties.js
@@ -1,10 +1,12 @@
 // Protect From errors
+let role = "IT Admin";
 
 let obj = {
 
     myName : "Sufyan Ali",
     age : 19,
     email : "test@test.com",
+    [role] : "Sufyan Ali", // Computed Properties
     addresss : {
 
     
