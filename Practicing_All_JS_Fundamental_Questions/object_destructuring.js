56f1bb86d74cf44bef9b480262cbe8ca46ad62de
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-05 15:18:36 +0500

    Object Destructuring

---
+++ Practicing_All_JS_Fundamental_Questions/object_destructuring.js
@@ -0,0 +1,13 @@
+let user = {
+    myName : "Sufyan Ali",
+    age : 19,
+    hobbies : "Problem Solving",
+    address : {
+        province : "Punjab",
+        city : "Rawalpindi"
+    }
+};
+
+let {province,city} = user.address;
+
+console.log(province,city );
