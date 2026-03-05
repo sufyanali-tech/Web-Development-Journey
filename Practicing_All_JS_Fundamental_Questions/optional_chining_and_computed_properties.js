73b2b444ec3fd79aa77714630b10641f6df8fe86
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-05 16:03:48 +0500

    Optional chaning in Js

---
+++ Practicing_All_JS_Fundamental_Questions/optional_chining_and_computed_properties.js
@@ -0,0 +1,15 @@
+// Protect From errors
+
+let obj = {
+
+    myName : "Sufyan Ali",
+    age : 19,
+    email : "test@test.com",
+    addresss : {
+
+    
+        city : "Punjab"
+    }
+}
+
+console.log(obj?.address?.city);
