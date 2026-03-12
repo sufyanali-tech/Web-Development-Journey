27fd165fe219df78afc68d4f121742b859cf999f
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-12 22:36:53 +0500

    Text Maupulation

---
+++ DOM_in_JS/text_manupulation/script.js
@@ -0,0 +1,19 @@
+//  dom manipulation
+// html se element select karna
+// text badalna 
+// html badalna
+// css badalna
+// attribute
+// event listners
+
+// Selecting Element
+
+// let h1 = document.querySelector("h1"); // selecting the element only select 1st h1 tag
+// h1.innerHTML = "Hi my name is Sufyan Ali"  
+
+let h1 = document.querySelector("h1");
+// h1.innerHTML = "<i> hi brother </>"
+// h1.hidden = true 
+
+console.dir(h1)
+
