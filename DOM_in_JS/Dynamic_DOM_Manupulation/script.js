5a349659a047adfc26d5269480b33e05073eb9c7
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-13 10:00:33 +0500

    Dynamic DOM with append and prepend in JS

---
+++ DOM_in_JS/Dynamic_DOM_Manupulation/script.js
@@ -0,0 +1,7 @@
+// creating the element
+// append /  prepend in the body
+
+let h1 = document.createElement("h1")
+h1.textContent = "Hi Hello From JS"
+
+document.querySelector("body").prepend(h1) // before script file append() after script file
