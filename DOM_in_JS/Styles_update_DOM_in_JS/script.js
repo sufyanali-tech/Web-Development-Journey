f82118ea91b50bc135c9b250630016053945bfaa
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-13 15:33:09 +0500

    ClassList in DOM JS

---
+++ DOM_in_JS/Styles_update_DOM_in_JS/script.js
@@ -0,0 +1,8 @@
+// JS say CSS badalna
+
+let h1 = document.querySelector("h1");
+
+// h1.classList.add("hello") // Adding the css class to the element 
+// h1.classList.remove("hello") // Adding the css class to the element 
+h1.classList.toggle("hello") // Adding the css class to the element 
+
