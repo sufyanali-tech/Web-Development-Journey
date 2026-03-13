2537757043963d86a7a359a5b2640a3f3c7a9acb
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-13 17:28:21 +0500

    1st Question

---
+++ DOM_in_JS/Practicing_DOM/script.js
@@ -0,0 +1,16 @@
+// 🟢 Level 1 — DOM Selection Practice
+// 1️⃣ Select by ID
+
+// Create HTML:
+
+// <h1 id="title">Hello World</h1>
+
+// Using JavaScript:
+
+// Select this element
+
+// Print its text in console.
+
+let h1 = document.querySelector("#title");
+
+console.log(h1.textContent);
