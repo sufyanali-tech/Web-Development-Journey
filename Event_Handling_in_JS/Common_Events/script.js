b5c0476f392ecfe77ddb4747722028cc5f10c750
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-14 11:13:49 +0500

    Change Event

--- Event_Handling_in_JS/Common_Events/script.js
+++ Event_Handling_in_JS/Common_Events/script.js
@@ -1,4 +1,4 @@
-// Add
+// Add Event 
 
 let inputField = document.querySelector("input");
 
@@ -6,9 +6,20 @@ function userInput(userValue) {
 
     if(userValue.data !== null) {
 
-        console.log(userValue);  
+        console.log(userValue.data);  
     }
     
 }
 
 inputField.addEventListener("input",userInput)
+
+// Change Event
+
+let sel = document.querySelector("select");
+let h3 = document.querySelector("h3")
+
+sel.addEventListener("change",function(value) {
+
+    h3.textContent = `${value.target.value} Device Selected`;
+    
+})
