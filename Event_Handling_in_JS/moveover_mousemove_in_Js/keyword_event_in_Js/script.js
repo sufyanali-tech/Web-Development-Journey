188e74a71c333c348d5786f759fe4b6069bdcfcb
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-18 06:32:44 +0500

    Mouseover and Mousemove in JS

---
+++ Event_Handling_in_JS/moveover_mousemove_in_Js/keyword_event_in_Js/script.js
@@ -0,0 +1,11 @@
+let main = document.querySelector("#main")
+
+main.addEventListener("mouseover",function() {
+
+    main.style.backgroundColor = "yellow";
+})
+main.addEventListener("mouseout",function() {
+
+    main.style.backgroundColor = "red";
+})
+
