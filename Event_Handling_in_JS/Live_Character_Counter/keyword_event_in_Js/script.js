b8b4d1128b8e06ceb6a358c135e027b87f5a5657
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-18 14:47:27 +0500

    Live Counter Created in JS

---
+++ Event_Handling_in_JS/Live_Character_Counter/keyword_event_in_Js/script.js
@@ -0,0 +1,21 @@
+let input = document.querySelector("input");
+let span = document.querySelector("span");
+
+
+input.addEventListener(
+    "input",
+    function() {
+
+        let left = 20 - input.value.length;
+        span.textContent = left;
+
+        if(left < 0) {
+
+            span.style.color = "red"
+        }
+        else {
+
+            span.style.color = "black"
+        }
+    }
+)
