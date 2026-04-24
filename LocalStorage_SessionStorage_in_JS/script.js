fc880184667314c0fbd3182bcac2de78f40b61d1
Sufyan Ali <sufyanali.dev@gmail.com>
2026-04-24 23:37:44 +0500

    Craeted New Files to start localStorage SessionStorage etc in JS

---
+++ LocalStorage_SessionStorage_in_JS/script.js
@@ -0,0 +1,20 @@
+// Set TimeOut 
+
+let setTimeOut = setTimeout(function() {
+
+    console.log("My name is Sufyan Ali");
+    
+},2000)
+
+// Set Intervel
+
+let setIntervalVariable = setInterval(function(){
+
+    console.log("Hello World");
+    
+},2000)
+
+// ClearInterval 
+
+clearInterval(setTimeOut)
+clearInterval(setIntervalVariable)
