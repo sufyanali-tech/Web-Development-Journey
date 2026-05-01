c3cf4ea8eb6925d96a000bb1e065869ea96b92c1
Sufyan Ali <sufyanali.dev@gmail.com>
2026-05-01 22:12:06 +0500

    Lexical and Dynamic Scoping in JS

--- Scope_Execution_Closures/script.js
+++ Scope_Execution_Closures/script.js
@@ -23,3 +23,33 @@ if(true) {
     let lastName = "Ali"
 }
 
+// Lexical and Dynamical Scoping in JS
+
+function abcd() {
+
+    let a = 20 // a is lexical scoped which is used in function efgh 
+    function efgh() {
+
+        console.log(a);
+    }
+    efgh()
+}
+
+abcd()  
+
+// But JS don't follow the dynamic scoping 
+let variable = 10
+
+function Sufyan() {
+ 
+    console.log(variable);  // Js is not dynamical Scoped language that why 10 is printing
+    
+}
+
+function Ali() {
+
+    let variable = 20;
+    Sufyan()
+}
+
+Ali()
