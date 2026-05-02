7633019a5de870daf42f21dac6a19ac7f3b7e346
Sufyan Ali <sufyanali.dev@gmail.com>
2026-05-02 11:37:43 +0500

    Complete cases of using this keyword in JS

--- This_keyword_in_Js/script.js
+++ This_keyword_in_Js/script.js
@@ -24,10 +24,67 @@ let myObj = {
     age : 20,
     sayName : function() {
 
-        console.log(this.name);  // refer to the object 
+        console.log(this);  // refer to the object 
     }
 }
 
 myObj.sayName()
 
 
+let Obj = {
+    name : "Sufyan Ali",
+    age : 20,
+    sayName : function() {
+
+        let arrowFnc = () => {
+            console.log(this); // also refer the object because arrow function take the value of parent which is method
+        };
+        arrowFnc();
+    }
+}
+
+Obj.sayName()
+
+
+let anotherObject = {
+    name : "Sufyan Ali",
+    age : 20,
+    sayName : function() {
+
+        function abcd() {
+
+            console.log(this); // refer to the window 
+            
+        } abcd()
+    }
+}
+ 
+anotherObject.sayName()
+
+
+let anotherObjectwithArrow = {
+    name : "Sufyan Ali",
+    age : 20,
+    sayName : () => {
+
+        console.log(this); // refer to the window because parent is anotherObjectwithArrow which is declare in global 
+        
+    }
+
+}
+
+anotherObjectwithArrow.sayName()
+
+// this keyword value in class 
+
+class Personal {
+
+    constructor() {
+
+        this.a = 20
+        console.log(this); // refer to the black object 
+        
+    }
+}
+
+let newObject = new Personal()
