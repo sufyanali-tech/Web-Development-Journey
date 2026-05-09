440985291f211e1ab8640ff96144063fabe48563
Sufyan Ali <sufyanali.dev@gmail.com>
2026-05-09 19:38:14 +0500

    Created the first page of chatapp

--- ChatApp_UI_Project/script.js
+++ ChatApp_UI_Project/script.js
@@ -5,27 +5,51 @@ let storeLoginInputFields = null
 
 loginButton.addEventListener("click", function () {
 
-    storeLoginInputFields = loginBanner.innerHTML
-    loginBanner.innerHTML = `
-    <div id="login-with-number" class="login-with-number">
-      <select id="country-select" >
-        <option value="">United States</option>
-        <option value="">Pakistan</option>
-        <option value="">Saudi Arabia</option>
-        <option value="">United Arab Emirate</option>
-        <option value="">Qatar</option>
-      </select>
+  storeLoginInputFields = loginBanner.innerHTML
+  loginBanner.innerHTML = `
+   <div id="login-with-number" class="login-with-number">
+      <button type="button" id="country-selection" class="country-selection">
+        <div class="left">
+          <div class="flag">pk</div>
+          <div class="flag-name">Pakistan</div>
+        </div>
+        <div class="down-side">▼</div>
+      </button>
+      <div
+        id="country-code-selection"
+        class="country-code-selection"
+        style="display: none"
+      >
+        <input type="text" id="search-bar" class="search-bar" />
+        <ul>
+          <li>Pakistan</li>
+          <li>Turkey</li>
+          <li>jhdsf</li>
+          <li>dsfs</li>
+          <li>fdsa</li>
+          <li>Pakistan</li>
+        </ul>
+      </div>
       <input
         id="telephone-number"
         class="telephone-number"
-        placeholder="Enter your phone number"
         type="tel"
         maxlength="15"
+        placeholder="Enter phone number"
       />
-       <div id="next-btn">
+      <div id="next-btn">
         <button type="button" id="next-button" class="next-button">Next</button>
-       </div>
+      </div>
     </div>`
 
+  let countrySelection = document.querySelector("#country-selection");
+  let telephoneNumber = document.querySelector("#telephone-number")
+  let nextButton = document.querySelector("#next-button")
+  let countryCodeSelection = document.querySelector("#country-code-selection")
+  countrySelection.addEventListener("click",function(e){
 
+    countryCodeSelection.style.display = "inline"
+    
+
+  })
 })
