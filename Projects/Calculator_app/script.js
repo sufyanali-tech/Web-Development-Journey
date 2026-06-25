af8a62658ac9204ec3464c1a1fe4bd9bd110e539
Sufyan Ali <sufyanali.dev@gmail.com>
2026-06-25 12:19:34 +0500

    Change the ParseFloat Logic

--- Projects/Calculator_app/script.js
+++ Projects/Calculator_app/script.js
@@ -2,71 +2,104 @@ let input = document.querySelector("#operend-one");
 let output = document.querySelector("#output");
 let buttons = document.querySelectorAll("button");
 
-let operent1 = "";
-let operent2 = null;
-let userValueStore = "";
-let operatorStore = "";
+let userInputStoring1 = "";
+let userInputStoring2 = null;
+let userInputStoringTemporery = "";
+let operatorStoring = "";
 
 buttons.forEach(function (btn) {
+
   btn.addEventListener("click", function () {
     if (["+", "-", "x", "÷", "mod"].includes(btn.textContent)) {
-      if (operent2) {
-        userValueStore = operent2;
-        operent2 = null;
-        operent1 = "";
+
+      if (userInputStoring2) {
+
+        userInputStoringTemporery = userInputStoring2;
+        userInputStoring2 = null;
+        userInputStoring1 = "";
         input.textContent = "0";
-      } else {
-        userValueStore = operent1;
-        operent1 = "";
+      }
+      else {
+        userInputStoringTemporery = userInputStoring1;
+        userInputStoring1 = "";
         input.textContent = "0";
       }
       if (btn.textContent === "+") {
-        operatorStore = btn.textContent;
-      } else if (btn.textContent === "-") {
-        operatorStore = btn.textContent;
-      } else if (btn.textContent === "x") {
-        operatorStore = btn.textContent;
-      } else if (btn.textContent === "÷") {
-        operatorStore = btn.textContent;
-      } else if (btn.textContent === "mod") {
-        operatorStore = btn.textContent;
-      } else {
-        operatorStore = btn.textContent;
+
+        operatorStoring = btn.textContent;
       }
-    } else if (btn.textContent === "AC") {
-      operent1 = "";
-      operent2 = null;
+      else if (btn.textContent === "-") {
+        operatorStoring = btn.textContent;
+      }
+      else if (btn.textContent === "x") {
+        operatorStoring = btn.textContent;
+      }
+      else if (btn.textContent === "÷") {
+        operatorStoring = btn.textContent;
+      }
+      else if (btn.textContent === "mod") {
+        operatorStoring = btn.textContent;
+      }
+      else {
+        operatorStoring = btn.textContent;
+      }
+    } 
+    else if (btn.textContent === "AC") {
+
+      userInputStoring1 = "";
+      userInputStoring2 = null;
       input.textContent = "0";
       output.textContent = "0";
-    } else if (btn.textContent === "=") {
-      if (userValueStore && operatorStore === "+") {
-        operent2 = parseInt(userValueStore) + parseInt(operent1);
-        output.textContent = operent2;
-      } else if (userValueStore && operatorStore === "-") {
-        operent2 = parseInt(userValueStore) - parseInt(operent1);
-        output.textContent = operent2;
-      } else if (userValueStore && operatorStore === "x") {
-        operent2 = parseInt(userValueStore) * parseInt(operent1);
-        output.textContent = operent2;
-      } else if (userValueStore && operatorStore === "÷") {
-        operent2 = parseInt(userValueStore) / parseInt(operent1);
-        output.textContent = operent2;
-      } else if (userValueStore && operatorStore === "mod") {
-        operent2 = parseInt(userValueStore) % parseInt(operent1);
-        output.textContent = operent2;
-      }
-    } else if (btn.textContent === "del") {
-      if (operent1 !== "") {
-        operent1 = operent1.slice(0, -1);
-        input.textContent = operent1;
+    }
+    else if (btn.textContent === "=") {
 
-        if (operent1 === "") {
+      if (userInputStoringTemporery && operatorStoring === "+") {
+
+        userInputStoring2 = parseFloat(userInputStoringTemporery) + parseFloat(userInputStoring1);
+        output.textContent = userInputStoring2;
+
+      }
+      else if (userInputStoringTemporery && operatorStoring === "-") {
+
+        userInputStoring2 = parseFloat(userInputStoringTemporery) - parseFloat(userInputStoring1);
+        output.textContent = userInputStoring2;
+
+      }
+      else if (userInputStoringTemporery && operatorStoring === "x") {
+
+        userInputStoring2 = parseFloat(userInputStoringTemporery) * parseFloat(userInputStoring1);
+        output.textContent = userInputStoring2;
+
+      }
+      else if (userInputStoringTemporery && operatorStoring === "÷") {
+
+        userInputStoring2 = parseFloat(userInputStoringTemporery) / parseFloat(userInputStoring1);
+        output.textContent = userInputStoring2;
+
+      }
+      else if (userInputStoringTemporery && operatorStoring === "mod") {
+
+        userInputStoring2 = parseFloat(userInputStoringTemporery) % parseFloat(userInputStoring1);
+        output.textContent = userInputStoring2;
+      }
+    }
+    else if (btn.textContent === "del") {
+
+      if (userInputStoring1 !== "") {
+
+        userInputStoring1 = userInputStoring1.slice(0, -1);
+        input.textContent = userInputStoring1;
+
+        if (userInputStoring1 === "") {
+
           input.textContent = "0";
         }
       }
-    } else {
-      operent1 += btn.textContent;
-      input.textContent = operent1;
+    }
+    else {
+
+      userInputStoring1 += btn.textContent;
+      input.textContent = userInputStoring1;
     }
   });
 });
