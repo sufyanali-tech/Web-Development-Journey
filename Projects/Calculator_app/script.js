311f2ebd6f3660d456fd4fdde516b18d87e95abf
Sufyan Ali <sufyanali.dev@gmail.com>
2026-06-25 11:53:27 +0500

    Remove the ParseFloat Part

--- Projects/Calculator_app/script.js
+++ Projects/Calculator_app/script.js
@@ -10,16 +10,12 @@ let operatorStore = "";
 buttons.forEach(function (btn) {
   btn.addEventListener("click", function () {
     if (["+", "-", "x", "÷", "mod"].includes(btn.textContent)) {
-
       if (operent2) {
-
         userValueStore = operent2;
         operent2 = null;
         operent1 = "";
         input.textContent = "0";
-      } 
-      else {
-        
+      } else {
         userValueStore = operent1;
         operent1 = "";
         input.textContent = "0";
@@ -37,91 +33,38 @@ buttons.forEach(function (btn) {
       } else {
         operatorStore = btn.textContent;
       }
-    } 
-    else if (btn.textContent === "AC") {
-
+    } else if (btn.textContent === "AC") {
       operent1 = "";
       operent2 = null;
       input.textContent = "0";
       output.textContent = "0";
-    } 
-    else if (btn.textContent === "=") {
-
-      if (userValueStore.includes(".") || operent1.includes(".")) {
-        
-        if (userValueStore && operatorStore === "+") {
-
-          operent2 =parseFloat(userValueStore) + parseFloat(operent1);
-          output.textContent = operent2;
-
-        } 
-        else if (userValueStore && operatorStore === "-") {
-
-          operent2 =parseFloat(userValueStore) - parseFloat(operent1);
-          output.textContent = operent2;
-
-        } 
-        else if (userValueStore && operatorStore === "x") {
-
-          operent2 =parseFloat(userValueStore) * parseFloat(operent1);
-          output.textContent = operent2;
-        } 
-        else if (userValueStore && operatorStore === "÷") {
-
-          operent2 =parseFloat(userValueStore) / parseFloat(operent1);
-          output.textContent = operent2;
-        } 
-        else if (userValueStore && operatorStore === "mod") {
-
-          operent2 =parseFloat(userValueStore) % parseFloat(operent1);
-          output.textContent = operent2;
-        }
+    } else if (btn.textContent === "=") {
+      if (userValueStore && operatorStore === "+") {
+        operent2 = parseInt(userValueStore) + parseInt(operent1);
+        output.textContent = operent2;
+      } else if (userValueStore && operatorStore === "-") {
+        operent2 = parseInt(userValueStore) - parseInt(operent1);
+        output.textContent = operent2;
+      } else if (userValueStore && operatorStore === "x") {
+        operent2 = parseInt(userValueStore) * parseInt(operent1);
+        output.textContent = operent2;
+      } else if (userValueStore && operatorStore === "÷") {
+        operent2 = parseInt(userValueStore) / parseInt(operent1);
+        output.textContent = operent2;
+      } else if (userValueStore && operatorStore === "mod") {
+        operent2 = parseInt(userValueStore) % parseInt(operent1);
+        output.textContent = operent2;
       }
-
-      else {
-
-        if (userValueStore && operatorStore === "+") {
-
-                operent2 = parseInt(userValueStore) + parseInt(operent1)
-                output.textContent = operent2
-            }
-            else if (userValueStore && operatorStore === "-") {
-
-                operent2 = parseInt(userValueStore) - parseInt(operent1)
-                output.textContent = operent2
-            }
-            else if (userValueStore && operatorStore === "x") {
-
-                operent2 = parseInt(userValueStore) * parseInt(operent1)
-                output.textContent = operent2
-            }
-            else if (userValueStore && operatorStore === "÷") {
-
-                operent2 = parseInt(userValueStore) / parseInt(operent1)
-                output.textContent = operent2
-            }
-            else if (userValueStore && operatorStore === "mod") {
-                
-                operent2 = parseInt(userValueStore) % parseInt(operent1)
-                output.textContent = operent2
-            }
-      }
-
-    } 
-    else if (btn.textContent === "del") {
-
+    } else if (btn.textContent === "del") {
       if (operent1 !== "") {
         operent1 = operent1.slice(0, -1);
         input.textContent = operent1;
 
         if (operent1 === "") {
-
           input.textContent = "0";
         }
       }
-    } 
-    else {
-
+    } else {
       operent1 += btn.textContent;
       input.textContent = operent1;
     }
